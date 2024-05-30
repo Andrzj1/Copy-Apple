485d359079c52bfb7ca71b420a65e62faee6a238
Andrzj1 <156209172+Andrzj1@users.noreply.github.com>
2024-05-30 11:52:40 +0300

    js_one

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt ("Как тебя зовут?");
+let age = prompt ("Сколько тебе лет?");
+let username = prompt ("Какой у тебя адрес аккаунта?");
+let massanger = prompt ("Какой любимый месенджер?");
+alert("Приятно познакомится! " + name + " " + age + "лет");
